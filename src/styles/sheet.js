139478f20c9a430b79c780ca0fc01a0b841354a2
Muhamad Gameel <mg0x16@gmail.com>
2020-11-07 03:18:38 +0200

    [styles] create simple sheet manager insert css rules to css style sheets

---
+++ src/styles/sheet.js
@@ -0,0 +1,17 @@
+const cache = {};
+const cssRules = [];
+const prefix = "_utk";
+let insert = rule => cssRules.push(rule);
+
+if (typeof document !== "undefined") {
+  const style = document.head.appendChild(document.createElement("style"));
+  const { sheet } = style;
+  style.id = "main";
+  insert = rule => {
+    cssRules.push(rule);
+    sheet.insertRule(rule, sheet.cssRules.length);
+  };
+}
+
+export { cache, prefix, cssRules, insert };
+
